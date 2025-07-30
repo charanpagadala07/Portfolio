4cf97f356abe13cd1160f95490ec3e692215f480
charanpagadala07 <charanpagadala2004@gmail.com>
2025-07-30 23:25:34 +0530

    removed sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,7 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-    org: "javascript-mastery",
-    project: "javascript-nextjs",
-  },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  }
-);
+export default nextConfig;
